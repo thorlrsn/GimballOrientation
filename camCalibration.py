dedd37265f16080e9ce7ca4eb9aee80127500b72
Jhammerh92 <46594000+Jhammerh92@users.noreply.github.com>
2019-11-22 17:04:21 +0100

    Add files via upload

--- camCalibration.py
+++ camCalibration.py
@@ -5,69 +5,88 @@ from os.path import isfile, join
 
 
 
-camFileNameSave = 'camMat03'
-maxSnap = 30
+camFileNameSave = 'camMatCircle03'
+maxSnap = 50
+pattern = "AsCircle Grid" # 'Checkerboard' eller 'AsCircle Grid'.
+# CB: afstand mellem intersect på checkerboard ELLER CG: horz/vert afstand mellem centrum af circler, ikke skrå/diagonal afstand.
+calibrationDimension = 0.100/3 # circleGrid fra http://opencv.willowgarage.com/ printet til a4 .
+patternSize = tuple((4,11)) #grid af indre hjoerner af checkerboard, eller antal rækker og "søjler" på circle grid.
 
-calibrationSquareDimension = 0.1330/5 # testgrid
-patternSize = tuple((4,3)) #grid af indre hjoerner af checkerboard
-
-#calibrationSquareDimension = 0.134/3 # mellemstort pattern i lab
-#patternSize = tuple((8,5)) #grid af indre hjoerner af checkerboard
-
-#calibrationSquareDimension = 0.0685/5 # lille pattern på iphone
-#patternSize = tuple((4,3)) #grid af indre hjoerner af checkerboard
-
-#calibrationSquareDimension = 0.13864/6  #meter, pattern i fuld skaerm på 15''
-#patternSize = tuple((6,12)) #antal indre hjoerner af checkerboard
-
+def objPoints (grid, cDimension, pSize):
+    if grid == "Checkerboard":
+        #prepare CB object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+        op = np.zeros((pSize[1]*pSize[0],3), np.float32)
+        op[:,:2] = np.mgrid[0:pSize[0],0:pSize[1]].T.reshape(-1,2)
+        op *= cDimension
+    elif grid == "AsCircle Grid":
+        # prepare ASym circle grid object points.
+        op = []
+        for i in range(pSize[1]):
+            for j in range(pSize[0]):
+                op.append(( (2*j + i%2)*cDimension/2,i*cDimension/2, 0) )
+        op= np.array(op).astype('float32')
+    return op
+      
+objp = objPoints(pattern,calibrationDimension,patternSize)
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((patternSize[1]*patternSize[0],3), np.float32)
-objp[:,:2] = np.mgrid[0:patternSize[0],0:patternSize[1]].T.reshape(-1,2)
-objp *= calibrationSquareDimension
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-print('Detecting checkerboard')
+print("Detecting", pattern)
 cap = cv2.VideoCapture(0) # 0 for intern webcam
 snap = 0
 #cv2.namedWindow('Pose Estimation',cv2.WINDOW_NORMAL)
-cv2.namedWindow('Calibration', cv2.WINDOW_NORMAL)
+cv2.namedWindow("Calibration", cv2.WINDOW_NORMAL)
 while (cap.isOpened()):
     ret1, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('Calibration', gray)
+    cv2.imshow("Calibration", gray)
     if cv2.waitKey(1) & 0xff == ord(' '):
         # Find the chess board corners
-        ret, corners = cv2.findChessboardCorners(gray, patternSize)
-        # If found, add object points, image points (after refining them)
-        if ret == True:
-            objpoints.append(objp)
+        if pattern == "Checkerboard":
+            ret, corners = cv2.findChessboardCorners(gray, patternSize)
+            # If found, add object points, image points (after refining them)
+        if pattern == "AsCircle Grid":
+            ret, corners  = cv2.findCirclesGrid(gray, patternSize, flags = cv2.CALIB_CB_ASYMMETRIC_GRID)
 
+        if ret:
+            objpoints.append(objp)
             corners2 = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
             imgpoints.append(corners2)
 
             # Draw and display the corners
             img = cv2.drawChessboardCorners(img, patternSize, corners2, ret)
             snap += 1
-            cv2.imshow('Calibration', img)
+            cv2.imshow("Calibration", img)
             cv2.waitKey(500)
-    if snap >= maxSnap:
+
+    if snap >= maxSnap: # gemmer calibrering når maxsnaps er opnået
         cv2.waitKey(1000)
         cap.release()
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
         np.savez(camFileNameSave,mtx=mtx,dist=dist,rvecs=rvecs,tvecs=tvecs)
         h,  w = img.shape[:2]
         newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h) ,1 , (w,h))
+        print("CALIBRATION DONE: Calibrationfile saved as" , camFileNameSave)
         break
-    if cv2.waitKey(45) & 0xff == ord('q'):
+    if cv2.waitKey(45) & 0xff == ord('q'): # stopper calibrering ved at trykke "q"
         cap.release()
         cv2.destroyAllWindows()
+        print("CALIBRATION STOPPED, No calibrationfile saved.")
         break
 
-print('CALIBRATION DONE: saved as' , camFileNameSave)
+
+# printer "calibreringsfejl"
+tot_error = 0
+
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+    tot_error += cv2.norm(imgpoints[i],imgpoints2, cv2.NORM_L2)/len(imgpoints2)
+    
+mean_error = tot_error/len(objpoints)
+
+print("total error of calibration: %f" % mean_error)
